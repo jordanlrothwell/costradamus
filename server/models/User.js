3e022add957ccd1cd998efa160fd5e5273152ddc
Jordan Rothwell <jordanlrothwell@gmail.com>
2022-05-08 12:54:32 +1000

    implemented user model and installed bcrypt as dependency

---
+++ server/models/User.js
@@ -0,0 +1,42 @@
+const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt");
+
+// schema corresponding to the document interface
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      "Please fill a valid email address",
+    ],
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
+});
+
+UserSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
+    const hashedPassword = await bcrypt.hash(this.password, 12);
+    this.password = hashedPassword;
+  }
+
+  next();
+});
+
+UserSchema.methods.isCorrectPassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+const User = model("User", UserSchema);
+
+module.exports = User;
