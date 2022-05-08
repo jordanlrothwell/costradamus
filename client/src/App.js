e05287c53a350220ead96ffcd02a501669e171d3
Jordan Rothwell <jordanlrothwell@gmail.com>
2022-05-08 12:47:49 +1000

    removed typescript due to dependency conflicts - added some (currently non-functioning) react-beautiful-dnd template components

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
+import DragList from "./Components/DragList";
+
 const client = new ApolloClient({
   uri: '/graphql',
   cache: new InMemoryCache(),
@@ -11,6 +13,7 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <Router>
+        <DragList />
       </Router>
     </ApolloProvider>
   );
