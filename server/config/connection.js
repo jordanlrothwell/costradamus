ab3cfed2ac88d9378b3a6729e19d7a12578267f2
Jordan Rothwell <jordanlrothwell@gmail.com>
2022-05-17 18:23:04 +1000

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/costradamus', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/costradamus-db', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
